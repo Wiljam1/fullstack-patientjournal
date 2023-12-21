3036c61d303f6ef92d4f24e83e323508af6dbc73
Wiljam Wilmi <wiljam.wilmi@hotmail.com>
2023-12-21 15:18:16 +0100

    Unit-testing for controller

---
+++ src/test/java/se/kth/wwnr/patientjournal/controller/PatientJournalControllerTest.java
@@ -0,0 +1,171 @@
+package se.kth.wwnr.patientjournal.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import se.kth.wwnr.patientjournal.model.Condition;
+import se.kth.wwnr.patientjournal.model.Encounter;
+import se.kth.wwnr.patientjournal.model.Observation;
+import se.kth.wwnr.patientjournal.services.ConditionService;
+import se.kth.wwnr.patientjournal.services.EncounterService;
+import se.kth.wwnr.patientjournal.services.ObservationService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class PatientJournalControllerTest {
+
+    @MockBean
+    private ObservationService observationService;
+    @MockBean
+    private EncounterService encounterService;
+    @MockBean
+    private ConditionService conditionService;
+
+    @Autowired
+    private PatientJournalController patientJournalController;
+
+    @Test
+    void getAllObservations() {
+        List<Observation> observations = Arrays.asList(new Observation(), new Observation());
+        when(observationService.getAll()).thenReturn(observations);
+
+        List<Observation> result = patientJournalController.getAllObservations();
+
+        assertEquals(observations, result);
+        verify(observationService, times(1)).getAll();
+    }
+
+    @Test
+    void getPatientObservations() {
+        Long patientId = 1L;
+        List<Observation> patientObservations = Arrays.asList(new Observation(), new Observation());
+        when(observationService.getByPatientId(patientId)).thenReturn(patientObservations);
+
+        List<Observation> result = patientJournalController.getPatientObservations(patientId);
+
+        assertEquals(patientObservations, result);
+        verify(observationService, times(1)).getByPatientId(patientId);
+    }
+
+    @Test
+    void getPerformerObservations() {
+        Long performerId = 1L;
+        List<Observation> performerObservations = Arrays.asList(new Observation(), new Observation());
+        when(observationService.getByPerformerId(performerId)).thenReturn(performerObservations);
+
+        List<Observation> result = patientJournalController.getPerformerObservations(performerId);
+
+        assertEquals(performerObservations, result);
+        verify(observationService, times(1)).getByPerformerId(performerId);
+    }
+
+    @Test
+    void createObservation() {
+        Observation inputObservation = new Observation();
+        when(observationService.create(any(Observation.class))).thenReturn(inputObservation);
+
+        Observation createdObservation = patientJournalController.createObservation(inputObservation);
+
+        assertNotNull(createdObservation);
+        verify(observationService, times(1)).create(inputObservation);
+    }
+
+    @Test
+    void getAllEncounters() {
+        List<Encounter> encounters = Arrays.asList(new Encounter(), new Encounter());
+        when(encounterService.getAll()).thenReturn(encounters);
+
+        List<Encounter> result = patientJournalController.getAllEncounters();
+
+        assertEquals(encounters, result);
+        verify(encounterService, times(1)).getAll();
+    }
+
+    @Test
+    void getPatientEncounters() {
+        Long patientId = 1L;
+        List<Encounter> patientEncounters = Arrays.asList(new Encounter(), new Encounter());
+        when(encounterService.getByPatientId(patientId)).thenReturn(patientEncounters);
+
+        List<Encounter> result = patientJournalController.getPatientEncounters(patientId);
+
+        assertEquals(patientEncounters, result);
+        verify(encounterService, times(1)).getByPatientId(patientId);
+    }
+
+    @Test
+    void getPractitionerEncounters() {
+        Long practitionerId = 1L;
+        List<Encounter> practitionerEncounters = Arrays.asList(new Encounter(), new Encounter());
+        when(encounterService.getByPractitionerId(practitionerId)).thenReturn(practitionerEncounters);
+
+        List<Encounter> result = patientJournalController.getPractitionerEncounters(practitionerId);
+
+        assertEquals(practitionerEncounters, result);
+        verify(encounterService, times(1)).getByPractitionerId(practitionerId);
+    }
+
+    @Test
+    void createEncounter() {
+        Encounter inputEncounter = new Encounter();
+        when(encounterService.create(any(Encounter.class))).thenReturn(inputEncounter);
+
+        Encounter createdEncounter = patientJournalController.createEncounter(inputEncounter);
+
+        assertNotNull(createdEncounter);
+        verify(encounterService, times(1)).create(inputEncounter);
+    }
+
+    @Test
+    void getAllConditions() {
+        List<Condition> conditions = Arrays.asList(new Condition(), new Condition());
+        when(conditionService.getAll()).thenReturn(conditions);
+
+        List<Condition> result = patientJournalController.getAllConditions();
+
+        assertEquals(conditions, result);
+        verify(conditionService, times(1)).getAll();
+    }
+
+    @Test
+    void getPatientConditions() {
+        Long patientId = 1L;
+        List<Condition> patientConditions = Arrays.asList(new Condition(), new Condition());
+        when(conditionService.getByPatientId(patientId)).thenReturn(patientConditions);
+
+        List<Condition> result = patientJournalController.getPatientConditions(patientId);
+
+        assertEquals(patientConditions, result);
+        verify(conditionService, times(1)).getByPatientId(patientId);
+    }
+
+    @Test
+    void getDoctorConditions() {
+        Long doctorId = 1L;
+        List<Condition> doctorConditions = Arrays.asList(new Condition(), new Condition());
+        when(conditionService.getByDoctorId(doctorId)).thenReturn(doctorConditions);
+
+        List<Condition> result = patientJournalController.getDoctorConditions(doctorId);
+
+        assertEquals(doctorConditions, result);
+        verify(conditionService, times(1)).getByDoctorId(doctorId);
+    }
+
+    @Test
+    void createCondition() {
+        Condition inputCondition = new Condition();
+        when(conditionService.create(any(Condition.class))).thenReturn(inputCondition);
+
+        Condition createdCondition = patientJournalController.createCondition(inputCondition);
+
+        assertNotNull(createdCondition);
+        verify(conditionService, times(1)).create(inputCondition);
+    }
+}
